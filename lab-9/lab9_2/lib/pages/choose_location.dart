fecf78ade75589b0f2f0db43201361df602dd828
Dev12122002 <devoza121220@gmail.com>
2022-09-18 11:07:24 +0530

    lab9 tutorial2 added

---
+++ lab-9/lab9_2/lib/pages/choose_location.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class ChooseLocation extends StatefulWidget {
+// const ChooseLocation({Key? key}) : super(key: key);
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose_location'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Text('CHOOSE LOCATION SCREEN'),
+    );
+  }
+}
